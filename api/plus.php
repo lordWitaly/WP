7cd74397be511153b247cd362be5d6fc3442e251
lordWitaly <witaly_pershakov@mail.ru>
2021-12-12 15:59:31 +0600

    dom z

--- api/plus.php
+++ api/plus.php
@@ -1,8 +1,9 @@
 <?php
-
+session_start();
 $x = $_REQUEST["x"];
 $y = $_REQUEST["y"];
 $z = $x + $y;
+$u = $_SESSION["user"];
 
 // Здесь нарушены принципы безопасности:
 // 1. Принцип наименьших привилегий
@@ -15,7 +16,7 @@ $z = $x + $y;
 include(getenv("MYAPP_CONFIG"));
 $conn = mysqli_connect($DB_URL,$DB_USER,$DB_PWD,$DB_NAME);
 
-$sql = "INSERT INTO log(Number1,Number2,Result,UserID) VALUES($x,$y,$z,'anonym')";
+$sql = "INSERT INTO log(Number1,Number2,Result,UserID) VALUES($x,$y,$z,$u)";
 mysqli_query($conn,$sql);
 //echo(mysqli_error($conn));
 mysqli_close($conn);
