7b2fd74d92790330c1721f75184f78581c0bccdf
lordWitaly <witaly_pershakov@mail.ru>
2021-12-18 20:27:09 +0600

    docker

--- include/params.php
+++ include/params.php
@@ -1,6 +1,10 @@
 <?php
-    $DB_URL ="localhost";
-    $DB_USER ="root";
-    $DB_PWD ="";
-    $DB_NAME ="calc";
+    $DB_URL = "yand.dyndns.org:33306";
+    $DB_USER = "calc";
+    $DB_PWD = "blaCalc60@yand";
+    $DB_NAME = "calc";
+    //$DB_URL ="localhost";
+    //$DB_USER ="root";
+    //$DB_PWD ="";
+    //$DB_NAME ="calc";
 ?>
--- tmp.php
+++ tmp.php
@@ -1,20 +1,24 @@
 <?php
-    session_start();
-    echo $_SESSION["user"];
-    
+session_start();
 $x = 1;
 $y = 2;
 $z = $x + $y;
 $u = $_SESSION["user"];
 
+// Здесь нарушены принципы безопасности:
+// 1. Принцип наименьших привилегий
+// 2. Слабый пароль
+// 3. Секрет в коде
+//$conn = mysqli_connect("localhost","root","","calc");
+// 4. Код, уязвимый для Sql-injection
 
-
+//Первые три проблемы исправлены ниже:
 include(getenv("MYAPP_CONFIG"));
 $conn = mysqli_connect($DB_URL,$DB_USER,$DB_PWD,$DB_NAME);
-//$sql = "INSERT INTO log(Number1,Number2,Result,UserID) VALUES($x,$y,$z,'uriy')";
+echo "INSERT INTO log(Number1,Number2,Result,UserID) VALUES($x,$y,$z,'$u')";
 $sql = "INSERT INTO log(Number1,Number2,Result,UserID) VALUES($x,$y,$z,'$u')";
 mysqli_query($conn,$sql);
 echo(mysqli_error($conn));
 mysqli_close($conn);
 echo($z);
-    ?>
+?>
